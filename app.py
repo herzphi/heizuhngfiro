f77c53d878328e3e7f6051c1a6196ead37635266
herzphi <philipp.herz@cellcentric.net>
2022-12-25 13:09:53 +0100

    Added plot and sidebar

--- app.py
+++ app.py
@@ -1,84 +1,85 @@
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
 import dash_daq as daq
 import plotly.express as px
+import plotly.graph_objects as go
 
-from iotcloudtemp.connect import get_temp_by_hour, get_connection
+from iotcloudtemp.connect import get_temp_by_hour, get_thing_id, checkboxes_table, revive_connection
 
-properties, thing_id, client_properties = get_connection()
+client_things, client_properties = revive_connection()
+thing_id, properties = get_thing_id(client_things, client_properties)
+df_propids, dict_propid_list = checkboxes_table(properties)
 
-name_props, id_props = ([] for i in range(2))
-for i in range(len(properties)):
-    name_props.append(properties[i].name)
-    id_props.append(properties[i].id)
 
 mathjax = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML'
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 server = app.server
 
-app.layout = html.Div(children=[
-    html.H1(children='Heizung FiRo'),
-    #html.Div(id='toggle-info'),
-    daq.ToggleSwitch(
-        value=False,
-        id='toggle-live-offline',
-        label='Live Updates'
-    ),
-    dcc.Dropdown(name_props, id='sensor-dropdown'),
-    html.Table(className='table_info',
-    children=[
-        html.Tr(
-        [
-            html.Td(
-                dcc.Markdown('''$T_{current} =$''', mathjax=True)
-            ),
-            html.Td(
-                html.Div(id='current_temperature')
-            ),
-        ]),
-        html.Tr([
-            html.Td(
-                dcc.Markdown('''$T_{mean} =$''', mathjax=True)
-            ),
-            html.Td(
-                html.Div(id='mean_temperature')
-            )
-        ]),
-        html.Tr([
-            html.Td(
-                dcc.Markdown('''$T_{max} =$''', mathjax=True)
-            ),
-            html.Td(
-                html.Div(id='max_temperature')
-            ),
-        ]),
-        html.Tr([
-            html.Td(
-                dcc.Markdown('''$T_{min} =$''', mathjax=True)
-            ),
-            html.Td(
-                html.Div(id='min_temperature')
-            ),
-        ])
-    ]),
-    html.Table([
-        html.Tr([
-            html.Td(
-                dcc.Graph(id='live-update-graph'),
-                style={'width':'80%'}
-            )
-        ]),
-    ]),
-    dcc.Interval(
-            id='interval-component',
-            interval=10*1000, # in milliseconds
-            n_intervals=0,
-            disabled=True
-        )
-]) 
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+    "font-size": "9pt",
+}
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
 
+sidebar = html.Div(
+    [
+        html.H2("Settings", className="display-4"),
+        html.Hr(),
+        html.P(
+            "Do it. Just do it.", className="lead"
+        ),
+        html.Div(id='current_temperature'),
+        dbc.Card(
+            [
+                daq.ToggleSwitch(
+                    value=False,
+                    id='toggle-live-offline',
+                    label='Live Updates'
+                ),
+                daq.ToggleSwitch(value=False, id='toggle-mean-date', label='Comparison On/Off'),
+                dcc.Checklist(
+                    options=dict_propid_list, 
+                    value=[dict_propid_list[0]['value']], 
+                    id='sensor-checklist',
+                    style={"width":"80%","display": "block"},
+                    labelStyle={"line-height":"2.1", "padding-left":"5px"},
+                    inputStyle={"margin-right":"5px"}
+                )
+            ],
+        ),
+    ],
+    style=SIDEBAR_STYLE)
 
+content = html.Div(children=[
+            html.H1(children='Heizung FiRo'),
+            dcc.Graph(
+                id='live-update-graph',
+            ),
+            dcc.Graph(
+                id='live-update-graph-stats',
+            ),
+            dcc.Interval(
+                    id='interval-component',
+                    interval=10*1000, # in milliseconds
+                    n_intervals=0,
+                    disabled=True
+                )
+        ], style=CONTENT_STYLE) 
+
+app.layout = html.Div([sidebar, content])
 
 
 
@@ -96,45 +97,55 @@ def update_output(value):
 
 @app.callback([
     Output('live-update-graph', 'figure'),
+    Output('live-update-graph-stats', 'figure'),
     Output('current_temperature', 'children'),
-    Output('mean_temperature', 'children'),
-    Output('max_temperature', 'children'),
-    Output('min_temperature', 'children'),
     ],
     [
     Input('interval-component', 'n_intervals'),
-    Input('sensor-dropdown', 'value')
+    Input('sensor-checklist', 'value'),
+    Input('toggle-mean-date', 'value'),
     ]
 )
-def update_graph_live(n):
-    df_data = get_temp_by_hour(properties, client_properties, thing_id)
-    latest_data, time, date = df_data.tail(1).value.values[0], \
-        df_data.tail(1).time.values[0], df_data.tail(1).date.values[0]
-    fig = px.line(df_data,
-                    x="hour", 
-                    y="value", 
-                    color='date',
-                    template='simple_white',
-                    labels={
-                        "hour": "Hour of the Day",
-                        "value": "Temperature in °C",
-                        "date": "Date (YYYY-MM-DD)"
-                    },
-    )
-    #fig.update_layout(showlegend=False)
-    values_series = df_data['value']
-    mean_T, std_T = values_series.mean(), values_series.std()
-    max_T = values_series.max()
-    min_T = values_series.min()
-    min_t_time = df_data[df_data['value']==min_T]['hour'].values[0]
-    if min_t_time<13:
-        ampm = 'am'
-    else:
-        min_t_time = 'pm'
-
-    return fig, f'{latest_data:.2f} °C at {time}', \
-        f'({mean_T:.1f} ± {std_T:.1f}) °C', f'{max_T:.1f} °C', \
-            f'{min_T:.1f} °C at {min_t_time:.0f} {ampm}'
+def update_graph_live(n, sensor, datecheck):
+    df_data, df_avg = get_temp_by_hour(sensor, client_properties, thing_id, df_propids)
+    latest_data, time = df_data.tail(1).value.values[0], \
+            df_data.tail(1).time.values[0]
+    purpose = df_propids['purpose'].values[0]
+    if str(datecheck) == 'True':
+        fig = px.line(df_data,
+                        x="hour",
+                        y="value", 
+                        color='date',
+                        template='simple_white',
+                        title='purpose',
+                        labels={
+                            "hour": "Hour of the Day",
+                            "value": "Temperature in °C",
+                            "date": "Date (YYYY-MM-DD)"
+                        },
+        )
+    elif str(datecheck) == 'False':
+        fig = px.scatter(
+            df_avg, 
+            x='hour', 
+            y='mean',
+            error_y='std',
+            color='purpose',
+            template='simple_white',
+        )
+        fig.update_layout(
+            xaxis_title_text='Hour of the Day',
+            yaxis_title_text='Mean Temperature in °C'
+        )
+        fig_stats = px.histogram(
+            df_data,
+            x="value",
+            template='simple_white',
+        )
+        fig_stats.update_layout(
+            xaxis_title_text='Temperature in °C'
+        )
+    return fig, fig_stats, f'T_current = {latest_data:.2f} °C at {time} ({purpose})'
 
 
 if __name__ == '__main__':
--- iotcloudtemp/connect.py
+++ iotcloudtemp/connect.py
@@ -47,26 +47,43 @@ def get_connection():
 
 
 
-def get_temp_by_hour(properties, client_properties, thing_id):
-    temp0_id = properties[0].id
-    to_date_dict = client_properties.properties_v2_timeseries(thing_id, temp0_id)
+def get_temp_by_hour(property_id, client_properties, thing_id, df_propids):
+    means, stds, hours, purposes = ([] for i in range(4))
+    for pid in property_id:
+        to_date_dict = client_properties.properties_v2_timeseries(thing_id, pid)
 
-    data_list = to_date_dict.data
-    times, values = ([] for i in range(2))
+        data_list = to_date_dict.data
+        times, values = ([] for i in range(2))
 
-    for el in data_list:
-        times.append(el.time+timedelta(hours=1))
-        values.append(el.value)
+        for el in data_list:
+            times.append(el.time+timedelta(hours=1))
+            values.append(el.value)
 
-    df_data = pd.DataFrame({
-        'datetime':times,
-        'value': values
-    })
-
-    df_data['date'] = pd.to_datetime(df_data['datetime']).dt.strftime('%d.%m.%Y')
-    df_data['time'] = pd.to_datetime(df_data['datetime']).dt.strftime('%H:%M')
-    df_data['hour'] = pd.to_datetime(df_data['datetime']).dt.hour + pd.to_datetime(df_data['datetime']).dt.minute/60
-    return df_data
+        df_data = pd.DataFrame({
+            'datetime':times,
+            'value': values
+        })
+        df_data['date'] = pd.to_datetime(df_data['datetime']).dt.strftime('%d.%m.%Y')
+        df_data['time'] = pd.to_datetime(df_data['datetime']).dt.strftime('%H:%M')
+        df_data['hour'] = pd.to_datetime(df_data['datetime']).dt.hour + pd.to_datetime(df_data['datetime']).dt.minute/60
+        df_data['hour_rounded'] = round(pd.to_datetime(df_data['datetime']).dt.hour + pd.to_datetime(df_data['datetime']).dt.minute/60)
+        df_data['purpose'] = df_propids[df_propids.id==pid].purpose
+        purpose = df_propids[df_propids.id==pid].purpose.values
+        #  Compile means and stddevs
+        for i in range(25):
+            mean = df_data[df_data['hour_rounded']==i]['value'].mean()
+            std = df_data[df_data['hour_rounded']==i]['value'].std()
+            hours.append(i)
+            means.append(mean)
+            stds.append(std)
+            purposes.append(purpose[0])
+    df_avg = pd.DataFrame({
+            'purpose': purposes,
+            'hour': hours,
+            'mean': means,
+            'std': stds,
+        })
+    return df_data, df_avg
 
 
 def get_live_temp(client_things, client_properties):
@@ -97,3 +114,32 @@ def revive_connection():
     client_properties = iot.PropertiesV2Api(client)
     client_things = iot.ThingsV2Api(client)
     return client_things, client_properties
+
+
+def get_thing_id(client_things, client_properties):
+    things = client_things.things_v2_list()
+    for i in range(len(things)):
+        if things[i].name=='Noel code':
+            thing_id = things[i].id
+    properties = client_properties.properties_v2_list(thing_id)
+    return thing_id, properties
+
+
+def checkboxes_table(properties):
+    df_propids = pd.DataFrame({
+        'name':[d.name for d in properties],
+        'id':[d.id for d in properties]
+    })
+    property_name = ['t_30','t_38','t_32','t_36','t_37','t_35','t_33','t_24','t_20','t_31','t_34','t_22','t_23','t_21']
+    property_purpose = ['WW-Zirkulation Rücklauf','WW-Zirkulation Vorlauf','Vorlauf Heizungsgerät',\
+        'Heizungsgerät Rücklauf','Vorlauf Wasserspeicher','Rücklauf Wasserspeicher','Küche Eltern',\
+            'Vorlauf Fussbodenheizung gesamt','Fussbodenheizung Vorlauf Bad','Heizung Vorlauf im Ölkeller',\
+                'Außentemperatur','Fussbodenheizung Vorlauf Wohnraum','Vorlauf Mixer Heizungsseitig','Rücklauf Mixer Heizungsseitig']
+    for name, purpose in zip(property_name, property_purpose):
+        row = df_propids[df_propids['name']==name].index
+        df_propids.loc[row, 'purpose'] = purpose
+    #  Make the dictionary
+    dict_propid_list = df_propids.drop('name', axis=1)\
+            .rename(columns={'id':'value', 'purpose':'label'})\
+                    .to_dict(orient='records')
+    return df_propids, dict_propid_list
